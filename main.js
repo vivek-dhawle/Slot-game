5f30afa519b97f9b26c4bf89ad6c36a8a0a2e22d
vivekdhawle <vivekdhawle02@gmail.com>
2026-01-28 11:27:11 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>PixiJS Basic Game</title>
+  
+</head>
+<body>
+
+  <script src="https://cdn.jsdelivr.net/npm/pixi.js@8/dist/pixi.min.js"></script>
+
+  <script  type='module' src="main.js"></script>
+
+</body>
+</html>
